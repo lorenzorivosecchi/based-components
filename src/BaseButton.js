550adbe783c1d5802b33175f9e70c6305350f875
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-12-28 22:09:46 +0100

    chore: use classNames helper function

--- src/BaseButton.js
+++ src/BaseButton.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import styles from './BaseButton.module.css'
+import { classNames } from "./lib/classNames";
 
 export default function BaseButton(props) {
   return (
     <button
-      className={[styles.button, props.className].join(' ')}
+      className={classNames(styles.button, props.className)}
       onClick={props.onClick}
     >
       {props.children}
       {props.tooltip && (
-        <div className={[styles.tooltip, props.tooltipClassName].join(' ')}>
+        <div className={classNames(styles.tooltip, props.tooltipClassName)}>
           {props.tooltip}
         </div>
       )}
