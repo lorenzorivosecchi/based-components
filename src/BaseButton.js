fe9f31e9f36e93517886f4038c39969f7c23e505
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-12-28 22:11:10 +0100

    refactor: enjoy the tests :-)

--- src/BaseButton.js
+++ src/BaseButton.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import styles from './BaseButton.module.css'
-import { classNames } from "./lib/classNames";
+import { classNames } from './lib/classNames'
 
 export default function BaseButton(props) {
+  const { className, onClick, tooltip, tooltipClassName, children } = props
+
   return (
     <button
-      className={classNames(styles.button, props.className)}
-      onClick={props.onClick}
+      className={classNames(styles.button, className)}
+      onClick={onClick}
     >
-      {props.children}
-      {props.tooltip && (
-        <div className={classNames(styles.tooltip, props.tooltipClassName)}>
-          {props.tooltip}
+      {children}
+      {tooltip && (
+        <div className={classNames(styles.tooltip, tooltipClassName)}>
+          {tooltip}
         </div>
       )}
     </button>
