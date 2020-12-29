baa6679f25c0f8c91398626dce902882a61594bd
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-12-29 22:22:33 +0100

    feat: pass onChange, onBLur and onFocus handlers from props

--- src/components/BaseCheckbox/index.js
+++ src/components/BaseCheckbox/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import styles from './index.module.css'
-import { classNames } from "../../lib/classNames";
+import { classNames } from '../../lib/classNames'
 
 export default function BaseCheckbox(props) {
-  const { className } = props;
+  const { className, onChange, onFocus, onBlur } = props
 
   return (
     <label className={classNames(styles.container, className)}>
-      <input type='checkbox' />
+      <input
+        type='checkbox'
+        onChange={onChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
+      />
       <span className={styles.checkmark}></span>
     </label>
   )
