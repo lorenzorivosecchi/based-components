7896268c90c55cd08af1a1f5fde51f82d16a9a09
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-12-29 22:46:56 +0100

    chore: pass value and event handlers

--- src/components/BaseTextField/index.js
+++ src/components/BaseTextField/index.js
@@ -3,8 +3,15 @@ import styles from './index.module.css'
 import { classNames } from '../../lib/classNames'
 
 export default function BaseTextField(props) {
-  const { className } = props
+  const { className, onChange, onFocus, onBlur, value } = props
   return (
-    <input type='text' className={classNames(styles.container, className)} />
+    <input
+      type='text'
+      className={classNames(styles.container, className)}
+      onChange={onChange}
+      onFocus={onFocus}
+      onBlur={onBlur}
+      value={value}
+    />
   )
 }
--- src/components/BaseTextField/index.test.js
+++ src/components/BaseTextField/index.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import BaseTextField from '.'
 import styles from './index.module.css'
 import { render } from '@testing-library/react'
-// import userEvent from '@testing-library/user-event'
+import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom/extend-expect'
 
 describe('BaseTextField', () => {
@@ -21,5 +21,33 @@ describe('BaseTextField', () => {
     const testClassName = 'secondary'
     const { container } = render(<BaseTextField className={testClassName} />)
     expect(container.firstChild).toHaveClass(styles.container, testClassName)
+  })
+  it('passes the onChange and value props', () => {
+    const handleChange = jest.fn()
+    const { getByRole } = render(<BaseTextField onChange={handleChange} />)
+    const textbox = getByRole('textbox')
+    userEvent.type(textbox, "hello")
+    expect(handleChange).toHaveBeenCalled()
+  })
+  it('passes the value prop', () => {
+    const testValue = "Hello";
+    const handleChange = jest.fn();
+    const { getByDisplayValue } = render(<BaseTextField value={testValue} onChange={handleChange} />)
+    expect(getByDisplayValue(testValue)).toBeDefined();
+  })
+  it('passes the onBlur prop', () => {
+    const handleBlur = jest.fn()
+    const { getByRole } = render(<BaseTextField onBlur={handleBlur} />)
+    const textbox = getByRole('textbox')
+    userEvent.tab(textbox)
+    userEvent.tab(textbox)
+    expect(handleBlur).toHaveBeenCalled()
+  })
+  it('passes the onFocus prop', () => {
+    const handleFocus = jest.fn()
+    const { getByRole } = render(<BaseTextField onFocus={handleFocus} />)
+    const textbox = getByRole('textbox')
+    userEvent.click(textbox)
+    expect(handleFocus).toHaveBeenCalled()
   })
 })
