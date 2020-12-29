4948a0ff61f67c40f0a6621476c48293a2cf9829
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-12-29 22:35:05 +0100

    chore: add extendable styles

--- src/components/BaseTextField/index.js
+++ src/components/BaseTextField/index.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React from 'react'
+import styles from './index.module.css'
+import { classNames } from '../../lib/classNames'
 
-export default function BaseTextField() {
-    return (
-        <input type="text" />
-    );
+export default function BaseTextField(props) {
+  const { className } = props
+  return (
+    <input type='text' className={classNames(styles.container, className)} />
+  )
 }
--- src/components/BaseTextField/index.test.js
+++ src/components/BaseTextField/index.test.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import BaseTextField from '.'
-// import styles from './index.module.css'
+import styles from './index.module.css'
 import { render } from '@testing-library/react'
 // import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom/extend-expect'
 
-
 describe('BaseTextField', () => {
   it('is truthy', () => {
-    expect(BaseTextField).toBeDefined();
+    expect(BaseTextField).toBeDefined()
   })
   it('renders a textbox', () => {
-      const { queryByRole } = render(<BaseTextField />)
-      expect(queryByRole("textbox")).not.toBeNull();
+    const { queryByRole } = render(<BaseTextField />)
+    expect(queryByRole('textbox')).not.toBeNull()
+  })
+  it('has default styles', () => {
+    const { container } = render(<BaseTextField />)
+    expect(container.firstChild).toHaveClass(styles.container)
+  })
+  it('merges classNames', () => {
+    const testClassName = 'secondary'
+    const { container } = render(<BaseTextField className={testClassName} />)
+    expect(container.firstChild).toHaveClass(styles.container, testClassName)
   })
 })
