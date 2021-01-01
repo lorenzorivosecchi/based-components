6ac5ef13d9fba28f9ab2f53d57604dac75bfca42
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2021-01-01 17:13:34 +0100

    chore: clean code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import React from 'react'
-import styles from './styles.module.css'
 import _BaseButton from "./components/BaseButton";
 import _BaseCheckbox from "./components/BaseCheckbox";
 import _BaseTextField from "./components/BaseTextField";
