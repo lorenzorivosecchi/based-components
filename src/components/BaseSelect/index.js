48bcef5d0aeecfe0c28eeaa84096a6ef68b02e48
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2021-01-01 21:44:09 +0100

    chore: use custom html to render select element

--- src/components/BaseSelect/index.js
+++ src/components/BaseSelect/index.js
@@ -5,12 +5,19 @@ export default function BaseSelect(props) {
   const { options = [] } = props
 
   return (
-    <select className={styles.wrapper} role='listbox'>
-      {options.map(({ label, value }) => (
-        <option key={value} value={value}>
-          {label}
-        </option>
-      ))}
-    </select>
+    <div className={styles.wrapper}>
+      <div role='listbox' className={styles.container}>
+        <div className={styles.trigger}>
+          <span>OPEN</span>
+        </div>
+        <div className={styles.options}>
+          {options.map(({ label, value }) => (
+            <span key={value} role="option" className={styles.option}>
+              {label}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
