fa2282bf52c7064c5cdd0c6ad46fea2daa175b72
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2021-01-04 22:14:16 +0100

    chore: add title

--- example/src/App.js
+++ example/src/App.js
@@ -1,14 +1,30 @@
 import React from 'react'
-import { BaseButton, BaseCheckbox, BaseTextField, BaseSelect } from 'based-components'
+import {
+  BaseButton,
+  BaseCheckbox,
+  BaseTextField,
+  BaseSelect
+} from 'based-components'
 import 'based-components/dist/index.css'
 
 const App = () => {
   return (
     <>
-      <BaseButton tooltip='Hello'>Click me.</BaseButton>
-      <BaseCheckbox />
-      <BaseTextField />
-      <BaseSelect options={["Red", "Green", "Yellow"]} />
+      <h1>Based Components</h1>
+      <ul>
+        <li>
+          <BaseButton tooltip='Hello'>Click me.</BaseButton>
+        </li>
+        <li>
+          <BaseCheckbox />
+        </li>
+        <li>
+          <BaseTextField />
+        </li>
+        <li>
+          <BaseSelect options={['Red', 'Green', 'Yellow']} />
+        </li>
+      </ul>
     </>
   )
 }
