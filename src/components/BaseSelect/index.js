dfcc807d0465d43c1e5fd2fbbc34b912ebc9e34c
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2021-01-06 22:20:07 +0100

    chore: reset BaseSelect component

--- src/components/BaseSelect/index.js
+++ src/components/BaseSelect/index.js
@@ -8,93 +8,14 @@ import { classNames } from '../../lib/classNames'
  * @see https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/listbox_role
  */
 export default function BaseSelect(props) {
-  const { options = [], onChange, multiple } = props
-
-  const [selection, setSelection] = useState([])
-  const [activeDescendant, setActiveDescendant] = useState()
-  const [open, setOpen] = useState(false)
-
-  const handleOptionClick = useCallback(
-    (event) => {
-      const value = event.target.innerHTML
-      const ariaSelected = event.target.getAttribute('aria-selected')
-
-      setActiveDescendant(event.target.id)
-
-      if (multiple) {
-        if (ariaSelected === 'true') {
-          setSelection(selection.filter((s) => s !== value))
-        } else {
-          setSelection([...selection, value])
-        }
-      } else {
-        setSelection([value])
-      }
-    },
-    [selection, setSelection]
-  )
-
-  const handleLabelClick = useCallback(() => {
-    setOpen(!open)
-  }, [open, setOpen])
-
-  const handleListboxFocus = useCallback((event) => {
-    const { target } = event
-    const options = target.children
-
-    if (options.length > 0) {
-      options[0].focus()
-    }
-  }, [])
-
-  useEffect(() => {
-    if (onChange) {
-      onChange(selection)
-    }
-  }, [selection])
-
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <div
-          role='label'
-          id={styles.label}
-          className={styles.label}
-          onClick={handleLabelClick}
-        >
-          Select
+    <div role='listbox' tabIndex={0}>
+      <div role='label'>Select</div>
+      {props.options.map((value, index) => (
+        <div role='option' key={value} tabIndex={1 + index}>
+          {value}
         </div>
-        <div
-          role='listbox'
-          tabIndex={0}
-          className={classNames(styles.options, open && styles.open)}
-          aria-activedescendant={activeDescendant}
-          aria-labelledby={styles.label}
-          aria-multiselectable={multiple}
-          onFocus={handleListboxFocus}
-        >
-          {options.map((value, index) => {
-            const isSelected = selection.some((s) => s === value)
-
-            return (
-              <div
-                key={value}
-                role='option'
-                id={`${styles.option}-${index}`}
-                tabIndex={1 + index}
-                className={classNames(
-                  styles.option,
-                  isSelected && styles.selected
-                )}
-                aria-selected={isSelected}
-                onClick={handleOptionClick}
-              >
-                {value}
-              </div>
-            )
-          })}
-        </div>
-      </div>
+      ))}
     </div>
   )
 }
--- src/components/BaseSelect/index.test.js
+++ src/components/BaseSelect/index.test.js
@@ -1,125 +1,39 @@
 import React from 'react'
 import BaseSelect from '.'
 import styles from './index.module.css'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom/extend-expect'
 
 describe('BaseSelect', () => {
+  const options = ['dog', 'cat', 'mouse']
+
+  beforeEach(() => {
+    render(<BaseSelect options={options} />)
+  })
+
   it('is truthy', () => {
     expect(BaseSelect).toBeTruthy()
   })
   it('renders a listbox', () => {
-    const { getByRole } = render(<BaseSelect />)
-    expect(getByRole('listbox')).toBeTruthy()
-  })
-  it('has default className', () => {
-    const { container } = render(<BaseSelect />)
-    expect(container.firstChild).toHaveClass(styles.wrapper)
+    expect(screen.getByRole('listbox')).toBeTruthy()
   })
   it('renders the options', () => {
-    const options = ['dog']
-    const { queryAllByRole } = render(<BaseSelect options={options} />)
-    expect(queryAllByRole('option')).toHaveLength(options.length)
+    screen.queryAllByRole('option').forEach((option, index) => {
+      expect(option).toHaveTextContent(options[index])
+    })
   })
   it('renders a label', () => {
-    const options = ['dog']
-    const { queryByRole } = render(<BaseSelect options={options} />)
-    expect(queryByRole('label')).not.toBeNull()
+    const label = screen.queryByRole('label');
+    expect(label).toHaveTextContent(/.+/);
   })
-  it('allows a single option to be selected at the time', () => {
-    const options = ['dog', 'cat']
-
-    const { getByRole, getAllByRole } = render(<BaseSelect options={options} />)
-
-    const listbox = getByRole('listbox')
-    const [first, second] = getAllByRole('option')
-
-    userEvent.click(first)
-    userEvent.click(second)
-
-    expect(first).toHaveAttribute('aria-selected', 'false')
-    expect(second).toHaveAttribute('aria-selected', 'true')
-    expect(listbox).toHaveAttribute('aria-activedescendant', second.id)
-  })
-  it('executes a callback when selection changes', () => {
-    const options = ['dog']
-    const handleChange = jest.fn()
-    const { getByRole } = render(
-      <BaseSelect options={options} onChange={handleChange} />
-    )
-    const firstOption = getByRole('option')
-
-    userEvent.click(firstOption)
-    expect(handleChange).toHaveBeenCalledWith([firstOption.innerHTML])
-  })
-  it('opens the options when label is clicked', () => {
-    const options = ['dog']
-    const { getByRole } = render(<BaseSelect options={options} />)
-
-    const label = getByRole('label')
-    const listbox = getByRole('listbox')
-
-    userEvent.click(label)
-    expect(listbox).toHaveClass(styles.open)
-  })
-  it('closes the options when label is clicked for the second time', () => {
-    const options = ['dog']
-    const { getByRole } = render(<BaseSelect options={options} />)
-
-    const label = getByRole('label')
-    const listbox = getByRole('listbox')
-
-    userEvent.click(label)
-    expect(listbox).toHaveClass(styles.open)
-    userEvent.click(label)
-    expect(listbox).not.toHaveClass(styles.open)
-  })
-  it('passes focus to the first item if there is no selection', () => {
-    const options = ['dog', 'cat', 'mouse']
-    const { getAllByRole } = render(<BaseSelect options={options} />)
-
-    const [ option ] = getAllByRole('option')
-
+  it('should cycle elements in tab order', () => {
     userEvent.tab()
-    expect(option).toHaveFocus()
-  })
-  describe('when multiple prop is set to true', () => {
-    it('specifies that is multiselectable', () => {
-      const options = ['dog']
-      const { getByRole } = render(
-        <BaseSelect options={options} multiple={true} />
-      )
-      expect(getByRole('listbox')).toHaveAttribute(
-        'aria-multiselectable',
-        'true'
-      )
-    })
-    it('allows multiple options to be selected', () => {
-      const options = ['dog', 'cat', 'mouse']
+    expect(screen.getByRole('listbox')).toHaveFocus()
 
-      const { getByRole, getAllByRole } = render(
-        <BaseSelect options={options} multiple={true} />
-      )
-
-      const listbox = getByRole('listbox')
-      const [first, second, third] = getAllByRole('option')
-
-      userEvent.click(first)
-      userEvent.click(second)
-
-      expect(first).toHaveAttribute('aria-selected', 'true')
-      expect(second).toHaveAttribute('aria-selected', 'true')
-      expect(third).toHaveAttribute('aria-selected', 'false')
-      expect(listbox).toHaveAttribute('aria-activedescendant', second.id)
-
-      userEvent.click(first)
-      userEvent.click(third)
-
-      expect(first).toHaveAttribute('aria-selected', 'false')
-      expect(second).toHaveAttribute('aria-selected', 'true')
-      expect(third).toHaveAttribute('aria-selected', 'true')
-      expect(listbox).toHaveAttribute('aria-activedescendant', third.id)
+    screen.getAllByRole('option').forEach((option) => {
+      userEvent.tab()
+      expect(option).toHaveFocus()
     })
   })
 })
