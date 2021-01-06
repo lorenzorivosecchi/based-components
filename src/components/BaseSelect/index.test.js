9a38da65bd111bb6885435c28b47394371857ff4
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2021-01-07 00:19:03 +0100

    chore: update test labels

--- src/components/BaseSelect/index.test.js
+++ src/components/BaseSelect/index.test.js
@@ -12,22 +12,22 @@ describe('BaseSelect', () => {
     render(<BaseSelect options={options} />)
   })
 
-  it('should be truthy', () => {
+  it('is be truthy', () => {
     expect(BaseSelect).toBeTruthy()
   })
-  it('should render a listbox', () => {
+  it('renders a listbox', () => {
     expect(screen.getByRole('listbox')).toBeTruthy()
   })
-  it('should render the options', () => {
+  it('renders the options', () => {
     screen.queryAllByRole('option').forEach((option, index) => {
       expect(option).toHaveTextContent(options[index])
     })
   })
-  it('should render a label', () => {
+  it('renders a label', () => {
     const label = screen.queryByRole('label')
     expect(label).toHaveTextContent(/.+/)
   })
-  it('should cycle elements in tab order', () => {
+  it('cycles elements in tab order', () => {
     userEvent.tab()
     expect(screen.getByRole('listbox')).toHaveFocus()
 
@@ -42,7 +42,7 @@ describe('BaseSelect', () => {
       userEvent.tab()
     })
 
-    it('should display the options', () => {
+    it('display the options', () => {
       expect(screen.getByRole('listbox')).toHaveClass(styles.open)
     })
   })
@@ -52,7 +52,7 @@ describe('BaseSelect', () => {
       userEvent.tab()
       userEvent.click(document.body)
     })
-    it('should hide the options', () => {
+    it('hides the options', () => {
       expect(screen.getByRole('listbox')).not.toHaveClass(styles.open)
     })
   })
@@ -61,11 +61,11 @@ describe('BaseSelect', () => {
       const options = screen.queryAllByRole('option')
       userEvent.click(options[0])
     })
-    it('should switch to selected', () => {
+    it('switches to selected', () => {
       const options = screen.queryAllByRole('option')
       expect(options[0]).toHaveAttribute('aria-selected', 'true')
     })
-    it('should be added as listbox active descendant', () => {
+    it('is added as listbox active descendant', () => {
       const options = screen.queryAllByRole('option')
       const listbox = screen.getByRole('listbox')
       expect(listbox).toHaveAttribute('aria-activedescendant', options[0].id)
@@ -78,11 +78,11 @@ describe('BaseSelect', () => {
       userEvent.click(options[0])
       userEvent.click(options[0])
     })
-    it('should switch to unselected', () => {
+    it('switches to unselected', () => {
       const options = screen.queryAllByRole('option')
       expect(options[0]).toHaveAttribute('aria-selected', 'false')
     })
-    it('should be removed from listbox active descendant', () => {
+    it('is removed from listbox active descendant', () => {
       const listbox = screen.getByRole('listbox')
       expect(listbox).not.toHaveAttribute('aria-activedescendant')
     })
@@ -94,12 +94,12 @@ describe('BaseSelect', () => {
       userEvent.click(options[0])
       userEvent.click(options[1])
     })
-    it('should override the previously selected option', () => {
+    it('overrides the previously selected option', () => {
       const options = screen.queryAllByRole('option')
       expect(options[0]).toHaveAttribute('aria-selected', 'false')
       expect(options[1]).toHaveAttribute('aria-selected', 'true')
     })
-    it('should replace the previous listbox active descendant', () => {
+    it('replaces the previous listbox active descendant', () => {
       const listbox = screen.getByRole('listbox')
       expect(listbox).toHaveAttribute('aria-activedescendant', options[1].id)
     })
@@ -116,7 +116,7 @@ describe('BaseSelect (multiple)', () => {
       userEvent.click(options[0])
       userEvent.click(options[1])
     })
-    it('should NOT override the previously selected option', () => {
+    it("doesn't override the previously selected option", () => {
       const options = screen.queryAllByRole('option')
       expect(options[0]).toHaveAttribute('aria-selected', 'true')
       expect(options[1]).toHaveAttribute('aria-selected', 'true')
